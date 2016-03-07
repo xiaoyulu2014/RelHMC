f9eb539c9ea3098b7805a55de14d7001a6968c5f
Xiaoyu Lu <xiaoyu_lu1992@yahoo.co.uk>
2016-03-07 23:40:21 +0000

    edit

--- examples/BananaExample.jl
+++ examples/BananaExample.jl
@@ -1,5 +1,7 @@
 @everywhere push!(LOAD_PATH,"/homes/xlu/Documents/RelHMC/src")
 @everywhere push!(LOAD_PATH,"/homes/xlu/Documents/RelHMC/models/")
+@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/src")
+@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/models/")
 @everywhere using SGMCMC
 @everywhere using DataModel
 @everywhere using Banana
@@ -20,14 +22,15 @@ end
     grad = getgrad(dm)
     llik = getllik(dm)
     samples = zeros(num_iterations, length(s.x))
+    accratio = zeros(num_iterations)
     zeta = zeros(num_iterations)
     for i = 1:num_iterations
 
-        sample!(s,llik,grad)
+        accratio[i]=sample!(s,llik,grad)[2]
         samples[i,:] = s.x
         if typeof(s) <: SGMCMC.SGNHTRelHMCState  zeta[i] = s.zeta[1]  end
     end
-     
+
     if final_plot
         if length(s.x) == 2
             figure()
@@ -39,7 +42,7 @@ end
             #plot(samples[:,1]);plot(samples[:,2]);title("traceplots of components")
         end
     end
-    samples
+    samples,accratio
 end
 
 ##function to plot ESS as the number of iterations
@@ -47,26 +50,50 @@ end
 	n,d=size(s)
 	ESS=Array(Float64,n,d)
 	for i=1:n
-       	   arf = StatsBase.autocor(s[1:i,:])
+       arf = StatsBase.autocor(s[1:i,:])
   	   ESS[i,:] = [i/(1+2*sum(arf[:,j])) for j=1:2]
 	end
 	if plot  plot(ESS) end
 	return(ESS)
 end
 
-
-
-stepsizevec = linspace(0.001,0.5,32);ESS=SharedArray(Float64,length(stepsizevec),2);rESS=SharedArray(Float64,length(stepsizevec),2)
-@sync @parallel for i=1:length(stepsizevec)
-	shmc = HMCState(zeros(2),stepsize=stepsizevec[i]);
-	hmc = run(shmc,dm,num_iterations=1000000, final_plot=false)
-        arf = StatsBase.autocor(hmc)
-	ESS[i,:] = [1000000/(1+2*sum(arf[:,j])) for j=1:size(hmc,2)]
+cvec=linspace(0.1,2,10);mvec=linspace(0.1,10,10);accvec=Array(Float64,10,10)
+for i=1:10
+  for j=1:10
+  srhmc = RelHMCState(zeros(2),stepsize=0.5,c = cvec[i],mass=mvec[j]);
+  rhmc,raccratio = run(srhmc,dm,num_iterations=10000, final_plot=false)
+  accvec[i,j]=mean(raccratio)
+end
 end
 
-outfile=open("banana_ESS","a") #append to file
-    println(outfile,"ESS=",ESS,"; stepsizevec=",stepsizevec)
-close(outfile)
+mvec=linspace(0.1,10,10);accvec=Array(Float64,10)
+for i=1:10
+  srhmc = RelHMCState(zeros(2),stepsize=0.5,mass=mvec[i]);
+  rhmc,raccratio = run(srhmc,dm,num_iterations=10000, final_plot=false)
+  accvec[i]=mean(raccratio)
+end
+
+
+stepsizevec = linspace(0.001,0.5,32);ESS=SharedArray(Float64,length(stepsizevec),2);accratiovec=SharedArray(Float64,length(stepsizevec))
+@sync @parallel for i=1:length(stepsizevec)
+	shmc = HMCState(zeros(2),stepsize=stepsizevec[i]);
+	hmc,accratio = run(shmc,dm,num_iterations=1000, final_plot=false)
+  arf = StatsBase.autocor(hmc)
+	ESS[i,:] = [1000/(1+2*sum(arf[:,j])) for j=1:size(hmc,2)]
+  accratiovec[i] = exp(mean(accratio))
+end
+plot(stepsizevec,ESS);
+plot(stepsizevec,accratiovec);
+
+stepsizevec = linspace(0.001,0.5,32);rESS=SharedArray(Float64,length(stepsizevec),2);raccratiovec=SharedArray(Float64,length(stepsizevec))
+@sync @parallel for i=1:length(stepsizevec)
+	srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i]);
+	rhmc,raccratio = run(srhmc,dm,num_iterations=1000, final_plot=false)
+  arf = StatsBase.autocor(rhmc)
+	rESS[i,:] = [1000/(1+2*sum(arf[:,j])) for j=1:size(rhmc,2)]
+  raccratiovec[i] = exp(mean(raccratio))
+end
+
 
 @sync @parallel for i=1:length(stepsizevec)
 	srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i]);
