0ac6fe45184d25c231af63d7f65faa8ff87d4f35
Xiaoyu Lu <xiaoyu_lu1992@yahoo.co.uk>
2016-03-08 23:29:54 +0000

    edit

--- examples/BananaExample.jl
+++ examples/BananaExample.jl
@@ -1,7 +1,7 @@
 @everywhere push!(LOAD_PATH,"/homes/xlu/Documents/RelHMC/src")
 @everywhere push!(LOAD_PATH,"/homes/xlu/Documents/RelHMC/models/")
-#@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/src")
-#@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/models/")
+@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/src")
+@everywhere push!(LOAD_PATH,"/Users/Xiaoyu Lu/Documents/RelHMC/models/")
 @everywhere using SGMCMC
 @everywhere using DataModel
 @everywhere using Banana
@@ -18,7 +18,7 @@
 
     PyPlot.contour(grid_x', grid_y', grid_f', 1)
 end
-@everywhere function run(s::SamplerState,dm::AbstractDataModel;num_iterations=1000, final_plot=false)
+@everywhere function myrun(s::SamplerState,dm::AbstractDataModel;num_iterations=1000, final_plot=false)
     grad = getgrad(dm)
     llik = getllik(dm)
     samples = zeros(num_iterations, length(s.x))
@@ -71,21 +71,24 @@ accvec=SharedArray(Float64,15,15);ESSvec=SharedArray(Float64,15,15)
   end
 end
 
-
-for i=1:5
+fig=figure()
+ax=fig[:add_axes]((0.1,0.1,0.9,0.9))
+ax[:set_ylim]([0,1])
+for i=1:15
+     ax[:set_ylim]([0,1])
        m=round(mvec[i],2)
        plot(cvec,accvec[:,i],label="m = $m")
 end
 legend(loc="lower left")
-xlabel("c");
+xlabel("c");axis([0,10,0,1])
 title("acceptance ratio vs c for different m, rHMC")
 
-for i=1:10
+for i=1:15
        c=round(cvec[i],2)
        plot(mvec,accvec[i,:][:],label="c = $c")
 end
 legend(loc="lower right")
-xlabel("m");
+xlabel("m");axis([0,5,0,1])
 title("acceptance ratio vs m for different c, rHMC")
 
 for i=1:15
@@ -96,7 +99,7 @@ legend(loc="upper right")
 xlabel("m");
 title("ESS vs m for different c, rHMC")
 
-for i=1:10
+for i=1:15
        m=round(mvec[i],2)
        plot(cvec,ESSvec[:,i],label="m = $m")
 end
@@ -108,13 +111,20 @@ title("ESS vs c for different m, rHMC")
     println(outfile,"accvec=",accvec,"; ESSvec=",ESSvec, "; cvec=", cvec, "; mvec=", mvec)
 close(outfile)
 =#
+#=
+outfile=open("rhmc_mc","a") #append to file
+    println(outfile,"accvec=",accvec,"; ESSvec=",ESSvec, "; cvec=", cvec, "; mvec=", mvec)
+close(outfile)
+=#
+
+#optimal m and c: c=9.3; m=0.4;
 
 
 
 mvec=linspace(0.1,10,10);accvec=Array(Float64,10)
 for i=1:10
   srhmc = RelHMCState(zeros(2),stepsize=0.5,mass=mvec[i]);
-  rhmc,raccratio = run(srhmc,dm,num_iterations=1000000, final_plot=false)
+  rhmc,raccratio = run(srhmc,dm,num_iterations=1000, final_plot=false)
   accvec[i]=mean(raccratio)
 end
 
@@ -123,9 +133,9 @@ end
 stepsizevec = linspace(0.001,0.5,32);ESS=SharedArray(Float64,length(stepsizevec),2);accratiovec=SharedArray(Float64,length(stepsizevec))
 @sync @parallel for i=1:length(stepsizevec)
 	shmc = HMCState(zeros(2),stepsize=stepsizevec[i]);
-	hmc,accratio = run(shmc,dm,num_iterations=1000, final_plot=false)
+	hmc,accratio = run(shmc,dm,num_iterations=1000000, final_plot=false)
   arf = StatsBase.autocor(hmc)
-	ESS[i,:] = [1000/(1+2*sum(arf[:,j])) for j=1:size(hmc,2)]
+	ESS[i,:] = [1000000/(1+2*sum(arf[:,j])) for j=1:size(hmc,2)]
   accratiovec[i] = exp(mean(accratio))
 end
 plot(stepsizevec,ESS);
@@ -133,13 +143,43 @@ plot(stepsizevec,accratiovec);
 
 stepsizevec = linspace(0.001,0.5,32);rESS=SharedArray(Float64,length(stepsizevec),2);raccratiovec=SharedArray(Float64,length(stepsizevec))
 @sync @parallel for i=1:length(stepsizevec)
-	srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i]);
-	rhmc,raccratio = run(srhmc,dm,num_iterations=1000, final_plot=false)
+	srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i],c=9.3, mass=0.4);
+	rhmc,raccratio = run(srhmc,dm,num_iterations=1000000, final_plot=false)
   arf = StatsBase.autocor(rhmc)
-	rESS[i,:] = [1000/(1+2*sum(arf[:,j])) for j=1:size(rhmc,2)]
+	rESS[i,:] = [1000000/(1+2*sum(arf[:,j])) for j=1:size(rhmc,2)]
   raccratiovec[i] = exp(mean(raccratio))
 end
 
+plot(stepsizevec,rESS)
+
+####animation
+
+
+
+function myani(s::SamplerState)
+  samples = myrun(s,dm,num_iterations=1000)[1]
+  llik = getllik(dm)
+  llik1(x,y) = llik([x,y])
+  range_x=-5:.05:6;range_y=-1:.05:32
+  grid_x = [i for i in range_x, j in range_y];
+  grid_y = [j for i in range_x, j in range_y];
+  grid_f = [exp(llik1(i,j)) for i in range_x, j in range_y];
+  fig=figure();
+  ax=fig[:add_subplot](111)
+  ax[:contour](grid_x',grid_y',grid_f',1)
+  line=ax[:plot](samples[1,1], samples[1,2],"ro")
+  for i=1:size(samples,1)
+    line[1][:set_data](samples[i,1], samples[i,2])
+    fig[:canvas][:draw]()
+  end
+end
+
+
+shmc = HMCState(zeros(2),stepsize=0.1);
+srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i],c=9.3, mass=0.4);
+
+myani(HMCState(zeros(2),stepsize=0.1))
+myani(RelHMCState(zeros(2),stepsize=0.1,c=9.3, mass=0.4))
 
 @sync @parallel for i=1:length(stepsizevec)
 	srhmc = RelHMCState(zeros(2),stepsize=stepsizevec[i]);
